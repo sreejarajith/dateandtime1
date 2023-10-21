202b81ff245b2abbfa7fb873df108805b80135b4
sreekutty87 <sreejacos2016@gmail.com>
2023-10-20 21:20:38 -0600

    date and time first commit

---
+++ src/main/java/dateandtime/DateAndTime.java
@@ -0,0 +1,35 @@
+package dateandtime;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import java.time.Period;
+
+import java.time.*;
+public class DateAndTime {
+
+public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+		//
+	System.out.println(LocalDate.now());
+	System.out.println(LocalTime.now());
+		
+	
+	
+	  LocalDate localdate=LocalDate.of(2023, 6, 1); LocalTime
+	  localtime=LocalTime.of(10, 30); System.out.println(localdate+","+localtime);
+	 
+	 
+		System.out.println(LocalDateTime.of(localdate, localtime));
+		//using period;
+		LocalDate start=LocalDate.of(2023, 10, 10);
+		LocalDate end=LocalDate.of(2023, 11, 10);
+Period period=Period.ofWeeks(1);
+//cleanHouse(start,end,period);
+System.out.println(start.plus(period));
+
+
+   
+
+	}
+}
+
